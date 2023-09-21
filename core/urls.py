99d096d2a802f553a88400b8e79d13f1ce6429c3
sanjuniperodee <87779571856b@gmail.com>
2023-09-21 20:18:06 +0600

    12=123ljkhgjkdfhg

--- core/urls.py
+++ core/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('get_restaurant_by_slug/<slug>', get_restaurant_by_slug, name='get_restaurant_by_slug'),
     path('create_certificate', create_certificate, name='create_certificate'),
     path('get_certificates_by_id/<id>', get_certificates_by_id, name='get_certificates_by_id'),
-    path('add_to_user_favourite/<userId>/<restaurantId>', add_to_user_favourite, name='add_to_user_favourite')
+    # path('add_to_user_favourite/<userId>/<restaurantId>', add_to_user_favourite, name='add_to_user_favourite')
 ]
